dbeb2cc4ef7a9e1e2400e9c3a31ecef16f49aafd
Reid Ashwill <reidashwill@gmail.com>
2020-06-23 16:10:47 -0700

    finish whiteboarding

--- euler.js
+++ euler.js
@@ -1,15 +1,56 @@
 
 
-const numberFinder = (num, totalArr = []) => {
-  if (num === 0) {
-    return totalArr.reduce(myFunc)
-  } else if (num % 3 === 0 || num % 5 === 0) {
-    totalArr.push(num)
-    return numberFinder(num - 1, totalArr);
-  } else {
-    return (numberFinder(num - 1, totalArr))
+// const numberFinder = (num, totalArr = []) => {
+//   if (num === 0) {
+//     return totalArr.reduce(myFunc)
+//   } else if (num % 3 === 0 || num % 5 === 0) {
+//     totalArr.push(num)
+//     return numberFinder(num - 1, totalArr);
+//   } else {
+//     return (numberFinder(num - 1, totalArr))
+//   }
+// }
+// function myFunc(total, num) {
+//   return total + num;
+// }
+
+const canPaint = function(color){
+  const obj = {
+    paints: function() {
+      return `paints ${color}.`
+    }
   }
+  return obj
 }
-function myFunc(total, num) {
-  return total + num;
+
+
+const painter1 = canPaint("Purple");
+
+painter1.paints()
+
+const makeNoise = function(noise){
+  const obj = {
+    sound: function() {
+      return `${noise}`
+    }
+  }
+  return obj
 }
+const faucet = makeNoise("Drip drip drip.")
+faucet.sound()
+const oldCar = makeNoise("Grumble grumble.")
+oldCar.sound()
+const sleepyBear = makeNoise("Grrr...yawn.")
+sleepyBear.sound()
+
+const warMachine = function(speed, distance) {
+  const obj = {
+    throw: function() {
+      return `The battle robot throws the spear ${distance} yards at ${speed} miles per hour`
+    }
+  }
+  return obj
+}
+
+const battlerobot1 = warMachine(200, 100)
+battlerobot1.throw()
